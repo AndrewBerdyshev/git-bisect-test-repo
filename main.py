d0dce63774974acca3a281c7bdb03fabf0431715
Andrew Berdyshev <an.berdyshev@gmail.com>
2025-02-10 22:29:41 +0700

    error 2

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 def sum(a, b):
     return a + b + 1
 
+def mult(a, b):
+    c = 0
+    for i in range(b):
+        c = sum(c, a)
+    return c
+
 def test1(capsys):
     assert sum(1, 2) == 3
+
+def test2(capsys):
+    assert mult(1, 2) == 2
 
 # python3 -m pytest main.py
